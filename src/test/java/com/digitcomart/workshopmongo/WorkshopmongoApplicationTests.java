9d89140c0cb1dd9f0b92944bc24d85c76e1b7e9d
Edson Marco <edin2008@yahoo.com.br>
2023-12-26 21:35:40 -0300

    projeto criado

---
+++ src/test/java/com/digitcomart/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitcomart.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
